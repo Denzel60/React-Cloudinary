5ecb6c5617f19fb0f12d7738b00e086539758503
Denzel <lawrencedenzel60@gmail.com>
2024-07-06 16:28:29 +0300

    Installed Axios and cloudinary, implemented a useState for sending the image to cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import Axios from 'axios'
+import { Image } from 'cloudinary-react'
 
 function App() {
 
+  const [imageSelected, setImageselected] = useState()
+
+  const uploadImage = () => {
+    // console.log(files[0])
+    const formData = new FormData()
+    formData.append('file', imageSelected)
+    formData.append('upload_preset', 'sfjaahz4')
+
+    Axios.post("https://api.cloudinary.com/v1_1/dsqpytomn/image/upload", formData).then((response) => {
+      console.log(response)
+    })
+  }
+
   return (
     <>
+      <input type="file" onChange={(event) => { setImageselected(event.target.files[0]) }} />
+      <button onClick={uploadImage}>Upload Image</button>
+
+      < Image cloudName="dsqpytomn" publicId="https://res.cloudinary.com/dsqpytomn/image/upload/v1720272139/u4tgurdui1fps0glxdyx.jpg" />
     </>
   )
 }
